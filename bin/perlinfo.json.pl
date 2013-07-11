d3e79f03b1f527c85ff39940c53d744115e321a1
Olivier Mengué <dolmen@cpan.org>
2013-07-11 21:23:47 +0200

    Report versions of loaded module at runtime
    This has not much performance impact as it implies no I/O at all
    ($VERSION is searched in the symbol table).

--- bin/perlinfo.json.pl
+++ bin/perlinfo.json.pl
@@ -4,10 +4,40 @@ use strict;
 use warnings;
 
 package App::PerlInfoJSON;
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 use Config::Perl::V ();
 use JSON::PP ();
+
+
+sub loaded_module_versions
+{
+    no strict 'refs';
+
+    my $module_versions = shift || {};
+    my $package = shift || '';
+    my $stash = shift || \%{$package.'::'};
+
+    my $glob;
+    if (exists $stash->{'VERSION'}
+	&& defined *{$glob = $stash->{'VERSION'}}{'SCALAR'}) {
+
+	$module_versions->{$package} = ${$glob} if defined ${$glob};
+    }
+
+    foreach my $k (grep { substr($_, -2) eq '::' } keys %$stash) {
+	next if "${package}::$k" eq '::main::';
+	loaded_module_versions(
+	    $module_versions,
+	    ($package ? ($package.'::'.substr($k, 0, -2)) : substr($k, 0, -2)),
+	    $stash->{$k});
+    }
+
+    return $module_versions
+}
+
+
+
 
 if ($ENV{GATEWAY_INTERFACE}) {
     print "Content-Type: application/json\015\012\015\012"
@@ -19,9 +49,11 @@ my $config = Config::Perl::V::myconfig();
 # so we remove it
 $config->{inc} = [ grep { ! ref } @{$config->{inc}} ];
 
+
 # Inject info about ourself
+# This gives some information about loaded modules without I/O cost
 $config->{+__PACKAGE__} = {
-    version => $VERSION,
+    versions => loaded_module_versions(),
 };
 
 print JSON::PP->new->ascii->pretty->encode($config);
